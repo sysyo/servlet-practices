f289c00213281df433e8f2010211555d1307989c
sysyo <youngiya77@gmail.com>
2021-10-08 12:02:48 +0900

    guestbook.sql

---
+++ docs/guestbook.sql
@@ -0,0 +1,17 @@
+desc guestbook;
+
+-- insert
+-- insert into guestbook values(null, '안대혁', '1234', '안녕하세요', now());
+insert into guestbook values(null, '둘리', '1234', '호이~', now());
+
+
+-- select
+select no, name, date_format(reg_date, '%Y/%m/%d %H:%i:%s'), message
+from guestbook
+order by reg_date desc;
+
+-- delete
+delete
+from guestbook
+where no = 1
+and password='1234';
